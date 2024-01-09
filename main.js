f5737acdb7c16c82ab555d1f32f3810f3e964c5c
DITSHOOT <54135074+DITSHOOT@users.noreply.github.com>
2024-01-09 18:50:23 +0100

    final edit

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
+/* 
+    Script jQuery.js pris sur CODEPEN (create)
+    Dimitri Chassignol (edit)
+
+*/
+
+// Attend que le document soit prêt avant d'exécuter le code (fonction draw() - ligne 36 HTML)
 $(document).ready(function() {
-    // $(document).draw();
+    // Animation de l'opacité et du margin du contenu principal
     $('#content').animate({
-        opacity:1,
-        marginTop:'0',
+        opacity: 1,
+        marginTop: '0',
     }, 800);
+
+    // Gestion du clic sur les titres h2
     $('h2').click(function() {
+        // Affiche ou masque le bloc de texte suivant au clic
         $(this).next('.subtext').slideToggle('fast');
+        
+        // Ajoute ou retire la classe 'moved' à l'élément .hex enfant du h2 cliqué
         $(this).children('.hex').toggleClass('moved');
-    })
+    });
 });
